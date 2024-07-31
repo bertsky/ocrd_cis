fed84da7c731c7e2ed3840122df7f5345c465534
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2024-07-31 15:59:17 +0200

    fix 53ae7d69 (already str not bytes)

--- ocrd_cis/ocrd_tool.py
+++ ocrd_cis/ocrd_tool.py
@@ -3,5 +3,4 @@ from ocrd_utils import resource_string
 
 
 def get_ocrd_tool():
-    return json.loads(
-        resource_string(__name__, 'ocrd-tool.json').decode('utf8'))
+    return json.loads(resource_string(__name__, 'ocrd-tool.json'))
