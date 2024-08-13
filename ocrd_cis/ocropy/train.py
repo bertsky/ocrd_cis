1b2fea3ed5b7c9d1a02f2dcabe0770aa3eb87da6
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-08-13 16:16:55 +0200

    refactor: log -> logger

--- ocrd_cis/ocropy/train.py
+++ ocrd_cis/ocropy/train.py
@@ -28,7 +28,7 @@ def resize_keep_ratio(image, baseheight=48):
 
 
 class OcropyTrain(Processor):
-    log: Logger
+    logger: Logger
 
     def __init__(self, *args, **kwargs):
         self.oldcwd = os.getcwd()
@@ -45,7 +45,7 @@ class OcropyTrain(Processor):
         return 'ocrd-cis-ocropy-train'
 
     def setup(self):
-        self.log = getLogger('processor.OcropyTrain')
+        self.logger = getLogger('processor.OcropyTrain')
         #print(self.parameter)
         if 'model' in self.parameter:
             model = self.parameter['model']
@@ -54,9 +54,9 @@ class OcropyTrain(Processor):
             except SystemExit:
                 ocropydir = os.path.dirname(os.path.abspath(__file__))
                 modelpath = os.path.join(ocropydir, 'models', model)
-                self.log.info("Failed to resolve model '%s' path, trying '%s'", model, modelpath)
+                self.logger.info("Failed to resolve model '%s' path, trying '%s'", model, modelpath)
             if not os.path.isfile(modelpath):
-                self.log.error("Could not find model '%s'. Try 'ocrd resmgr download ocrd-cis-ocropy-recognize %s'",
+                self.logger.error("Could not find model '%s'. Try 'ocrd resmgr download ocrd-cis-ocropy-recognize %s'",
                                model, model)
                 sys.exit(1)
             outputpath = os.path.join(self.oldcwd, 'output', model)
@@ -78,18 +78,18 @@ class OcropyTrain(Processor):
         """
         filelist = []
         filepath = tempfile.mkdtemp(prefix='ocrd-cis-ocropy-train-')
-        #self.log.info("Using model %s in %s for recognition", model)
+        #self.logger.info("Using model %s in %s for recognition", model)
         for (n, input_file) in enumerate(self.input_files):
-            #self.log.info("INPUT FILE %i / %s", n, input_file)
+            #self.logger.info("INPUT FILE %i / %s", n, input_file)
             pcgts = page_from_file(self.workspace.download_file(input_file))
             page_id = pcgts.pcGtsId or input_file.pageId or input_file.ID # (PageType has no id)
             page = pcgts.get_Page()
             page_image, page_coords, _ = self.workspace.image_from_page(page, page_id)
 
-            self.log.info("Extracting from page '%s'", page_id)
+            self.logger.info("Extracting from page '%s'", page_id)
             for region in page.get_AllRegions(classes=['Text']):
                 textlines = region.get_TextLine()
-                self.log.info("Extracting %i lines from region '%s'", len(textlines), region.id)
+                self.logger.info("Extracting %i lines from region '%s'", len(textlines), region.id)
                 for line in textlines:
                     if self.parameter['textequiv_level'] == 'line':
                         path = os.path.join(filepath, page_id + region.id + line.id)
@@ -110,7 +110,7 @@ class OcropyTrain(Processor):
                             if imgpath:
                                 filelist.append(imgpath)
 
-        self.log.info("Training %s from %s on %i file pairs",
+        self.logger.info("Training %s from %s on %i file pairs",
                       self.outputpath,
                       self.modelpath or 'scratch',
                       len(filelist))
@@ -130,7 +130,7 @@ class OcropyTrain(Processor):
         with open(gtpath, "w", encoding='utf-8') as f:
             f.write(gt)
 
-        self.log.debug("Extracting %s '%s'", segment.__class__.__name__, segment.id)
+        self.logger.debug("Extracting %s '%s'", segment.__class__.__name__, segment.id)
         image, coords = self.workspace.image_from_segment(segment, page_image, page_coords)
 
         if 'binarized' not in coords['features'].split(','):
