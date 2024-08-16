7c9f39fa4516401fe17e24d3ca67799c5b85d308
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2024-08-16 02:40:41 +0200

    deskew: adapt to final v3 API

--- ocrd_cis/ocropy/deskew.py
+++ ocrd_cis/ocropy/deskew.py
@@ -1,23 +1,20 @@
 from __future__ import absolute_import
+
+from typing import Optional
 from logging import Logger
 from os.path import join
 
-from ocrd_utils import (
-    getLogger,
-    make_file_id,
-    MIMETYPE_PAGE
-)
-from ocrd_modelfactory import page_from_file
+from ocrd_utils import getLogger
 from ocrd_models.ocrd_page import (
     PageType,
-    to_xml, AlternativeImageType
+    AlternativeImageType,
+    OcrdPage
 )
 from ocrd import Processor
+from ocrd.processor import OcrdPageResult, OcrdPageResultImage
 
 from . import common
 from .common import pil2array
-
-#sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 def deskew(pil_image, maxskew=2):
     array = pil2array(pil_image)
@@ -34,10 +31,10 @@ class OcropyDeskew(Processor):
     def setup(self):
         self.logger = getLogger('processor.OcropyDeskew')
 
-    def process(self):
+    def process_page_pcgts(self, *input_pcgts : Optional[OcrdPage], page_id : Optional[str] = None) -> OcrdPageResult:
         """Deskew the pages or regions of the workspace.
 
-        Open and deserialise PAGE input files and their respective images,
+        Open and deserialise PAGE input file and its respective images,
         then iterate over the element hierarchy down to the TextRegion level.
 
         Next, for each file, crop each region image according to the layout
@@ -53,62 +50,45 @@ class OcropyDeskew(Processor):
         Produce a new output file by serialising the resulting hierarchy.
         """
         level = self.parameter['level-of-operation']
+        pcgts = input_pcgts[0]
+        result = OcrdPageResult(pcgts)
+        page = pcgts.get_Page()
 
-        for (n, input_file) in enumerate(self.input_files):
-            self.logger.info("INPUT FILE %i / %s", n, input_file.pageId or input_file.ID)
-            file_id = make_file_id(input_file, self.output_file_grp)
-
-            pcgts = page_from_file(self.workspace.download_file(input_file))
-            self.add_metadata(pcgts)
-            page_id = pcgts.pcGtsId or input_file.pageId or input_file.ID # (PageType has no id)
-            page = pcgts.get_Page()
-                
-            page_image, page_coords, _ = self.workspace.image_from_page(
-                page, page_id,
+        page_image, page_coords, _ = self.workspace.image_from_page(
+            page, page_id,
+            # image must not have been rotated already,
+            # (we will overwrite @orientation anyway,)
+            # abort if no such image can be produced:
+            feature_filter='deskewed' if level == 'page' else '')
+        if level == 'page':
+            image = self._process_segment(page, page_image, page_coords, "page '%s'" % page_id, page_id)
+            if image:
+                result.images.append(image)
+            return result
+        if level == 'table':
+            regions = page.get_TableRegion()
+        else: # region
+            regions = page.get_AllRegions(classes=['Text'], order='reading-order')
+        if not regions:
+            self.logger.warning('Page "%s" contains no text regions', page_id)
+        for region in regions:
+            # process region:
+            region_image, region_coords = self.workspace.image_from_segment(
+                region, page_image, page_coords,
                 # image must not have been rotated already,
                 # (we will overwrite @orientation anyway,)
                 # abort if no such image can be produced:
-                feature_filter='deskewed' if level == 'page' else '')
-            if level == 'page':
-                self._process_segment(page, page_image, page_coords,
-                                      "page '%s'" % page_id, input_file.pageId,
-                                      file_id)
-            else:
-                if level == 'table':
-                    regions = page.get_TableRegion()
-                else: # region
-                    regions = page.get_AllRegions(classes=['Text'], order='reading-order')
-                if not regions:
-                    self.logger.warning('Page "%s" contains no text regions', page_id)
-                for region in regions:
-                    # process region:
-                    region_image, region_coords = self.workspace.image_from_segment(
-                        region, page_image, page_coords,
-                        # image must not have been rotated already,
-                        # (we will overwrite @orientation anyway,)
-                        # abort if no such image can be produced:
-                        feature_filter='deskewed')
-                    self._process_segment(region, region_image, region_coords,
-                                          "region '%s'" % region.id, input_file.pageId,
-                                          file_id + '_' + region.id)
+                feature_filter='deskewed')
+            image = self._process_segment(region, region_image, region_coords,
+                                          "region '%s'" % region.id, page_id)
+            if image:
+                result.images.append(image)
+        return result
 
-            # update METS (add the PAGE file):
-            file_path = join(self.output_file_grp, file_id + '.xml')
-            pcgts.set_pcGtsId(file_id)
-            out = self.workspace.add_file(
-                ID=file_id,
-                file_grp=self.output_file_grp,
-                pageId=input_file.pageId,
-                local_filename=file_path,
-                mimetype=MIMETYPE_PAGE,
-                content=to_xml(pcgts))
-            self.logger.info('created file ID: %s, file_grp: %s, path: %s',
-                     file_id, self.output_file_grp, out.local_filename)
-
-    def _process_segment(self, segment, segment_image, segment_coords, segment_id, page_id, file_id):
+    def _process_segment(self, segment, segment_image, segment_coords, segment_id, page_id) -> Optional[OcrdPageResultImage]:
         if not segment_image.width or not segment_image.height:
             self.logger.warning("Skipping %s with zero size", segment_id)
-            return
+            return None
         angle0 = segment_coords['angle'] # deskewing (w.r.t. top image) already applied to segment_image
         self.logger.info("About to deskew %s", segment_id)
         angle = deskew(segment_image, maxskew=self.parameter['maxskew']) # additional angle to be applied
@@ -123,20 +103,18 @@ class OcropyDeskew(Processor):
             segment_image, segment_coords, _ = self.workspace.image_from_page(
                 segment, page_id,
                 fill='background', transparency=True)
+            suffix = '.IMG-DESKEW'
         else:
             segment_image, segment_coords = self.workspace.image_from_segment(
                 segment, segment_image, segment_coords,
                 fill='background', transparency=True)
+            suffix = segment.id + '.IMG-DESKEW'
         if not angle:
             # zero rotation does not change coordinates,
             # but assures consuming processors that the
             # workflow had deskewing
             segment_coords['features'] += ',deskewed'
-        # update METS (add the image file):
-        file_path = self.workspace.save_image_file(
-            segment_image, file_id + '.IMG-DESKEW', self.output_file_grp,
-            page_id=page_id)
         # update PAGE (reference the image file):
-        segment.add_AlternativeImage(AlternativeImageType(
-            filename=file_path,
-            comments=segment_coords['features']))
+        alternative = AlternativeImageType(comments=segment_coords['features'])
+        segment.add_AlternativeImage(alternative)
+        return OcrdPageResultImage(segment_image, suffix, alternative)
