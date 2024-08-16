df1c35cbe1325a8da5dabd2c9227a7246439fd15
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2024-08-16 14:42:57 +0200

    train: adapt to final v3 API

--- ocrd_cis/ocropy/train.py
+++ ocrd_cis/ocropy/train.py
@@ -1,12 +1,15 @@
 from __future__ import absolute_import
+
+from typing import Optional
 from logging import Logger
 from sys import exit
 from os import getcwd, makedirs, remove
 from os.path import abspath, dirname, exists, join, isfile
 import tempfile
 
-from ocrd_modelfactory import page_from_file
-from ocrd import Processor
+from ocrd_models import OcrdPage
+from ocrd import Processor, Workspace
+from ocrd.processor import OcrdPageResult
 from ocrd_utils import getLogger
 
 from .ocropus_rtrain import *
@@ -37,80 +40,79 @@ class OcropyTrain(Processor):
 
     def setup(self):
         self.logger = getLogger('processor.OcropyTrain')
-        self.old_cwd = getcwd()
-        #print(self.parameter)
         if 'model' in self.parameter:
             model = self.parameter['model']
             try:
-                modelpath = self.resolve_resource(model)
+                self.modelpath = self.resolve_resource(model)
             except SystemExit:
                 ocropydir = dirname(abspath(__file__))
-                modelpath = join(ocropydir, 'models', model)
-                self.logger.info("Failed to resolve model '%s' path, trying '%s'", model, modelpath)
+                self.modelpath = join(ocropydir, 'models', model)
+                self.logger.error(f"Failed to resolve model '{model}' path, trying '{modelpath}'")
             if not isfile(modelpath):
-                self.logger.error("Could not find model '%s'. Try 'ocrd resmgr download ocrd-cis-ocropy-recognize %s'",
-                               model, model)
+                self.logger.critical(f"Could not find model '{model}'.\n"
+                                     f"Try 'ocrd resmgr download ocrd-cis-ocropy-recognize {model}'")
                 exit(1)
-            outputpath = join(self.old_cwd, 'output', model)
-            if 'outputpath' in self.parameter:
-                outputpath = join(self.parameter, model)
+            self.outputpath = join(self.parameter.get('outputpath', 'output'), model)
         else:
-            modelpath = None
-            outputpath = join(self.old_cwd, 'output', 'lstm')
-            if 'outputpath' in self.parameter:
-                outputpath = join(self.parameter, 'lstm')
-        makedirs(dirname(outputpath))
-        self.modelpath = modelpath
-        self.outputpath = outputpath
+            self.modelpath = None
+            self.outputpath = join(self.parameter.get('outputpath', 'output'), 'lstm')
+        makedirs(dirname(self.outputpath))
+        self.filelist = None
 
-    def process(self):
+    def process_workspace(self, workspace: Workspace) -> None:
         """
         Trains a new model on the text lines from the input fileGrp,
-        extracted as temporary image-text file pairs.
+        extracted as image-text file pairs into the output fileGrp.
+        (If the output fileGrp already exists and these files should
+        be re-used, pass the `--overwrite` option when processing.)
+
+        The model is written into `outputpath` (or just `output`) under
+        the same name as `model` (i.e. the start model, or just `lstm`).
         """
-        filelist = []
-        filepath = tempfile.mkdtemp(prefix='ocrd-cis-ocropy-train-')
+        self.filelist = []
+        super().process_workspace(workspace)
+        self.logger.info(f"Training {self.outputpath} from {self.modelpath or 'scratch'} "
+                         f"on {len(self.filelist)} file pairs")
+        rtrain(self.filelist, self.modelpath, self.outputpath, self.parameter['ntrain'])
+        # deletefiles(self.filelist)
+        
+    def process_page_pcgts(self, *input_pcgts : Optional[OcrdPage], page_id : Optional[str] = None) -> OcrdPageResult:
+        """
+        Extracts pairs of plaintext and cropped image files for each text line
+        in the PAGE file (to be used during training).
+        """
+        pcgts = input_pcgts[0]
         #self.logger.info("Using model %s in %s for recognition", model)
-        for (n, input_file) in enumerate(self.input_files):
-            #self.logger.info("INPUT FILE %i / %s", n, input_file)
-            pcgts = page_from_file(self.workspace.download_file(input_file))
-            page_id = pcgts.pcGtsId or input_file.pageId or input_file.ID # (PageType has no id)
-            page = pcgts.get_Page()
-            page_image, page_coords, _ = self.workspace.image_from_page(page, page_id)
+        page = pcgts.get_Page()
+        page_image, page_coords, _ = self.workspace.image_from_page(page, page_id)
 
-            self.logger.info("Extracting from page '%s'", page_id)
-            for region in page.get_AllRegions(classes=['Text']):
-                textlines = region.get_TextLine()
-                self.logger.info("Extracting %i lines from region '%s'", len(textlines), region.id)
-                for line in textlines:
-                    if self.parameter['textequiv_level'] == 'line':
-                        path = join(filepath, page_id + region.id + line.id)
-                        imgpath = self.extract_segment(path, line, page_image, page_coords)
-                        if imgpath:
-                            filelist.append(imgpath)
+        self.logger.debug(f"Extracting from page '{page_id}'")
+        for region in page.get_AllRegions(classes=['Text']):
+            textlines = region.get_TextLine()
+            self.logger.debug(f"Extracting {len(textlines)} lines from region '{region.id}'")
+            for line in textlines:
+                if self.parameter['textequiv_level'] == 'line':
+                    path = join(self.output_file_grp, f"{page_id}_{region.id}_{line.id}")
+                    self.filelist.append(self.extract_segment(path, line, page_image, page_coords))
+                    continue
+                for word in line.get_Word():
+                    if self.parameter['textequiv_level'] == 'word':
+                        path = join(self.output_file_grp, f"{page_id}_{region.id}_{line.id}_{word.id}")
+                        self.filelist.append(self.extract_segment(path, word, page_image, page_coords))
                         continue
-                    for word in line.get_Word():
-                        if self.parameter['textequiv_level'] == 'word':
-                            path = join(filepath, page_id + region.id + line.id + word.id)
-                            imgpath = self.extract_segment(path, word, page_image, page_coords)
-                            if imgpath:
-                                filelist.append(imgpath)
-                            continue
-                        for glyph in word.get_Glyph():
-                            path = join(filepath, page_id + region.id + line.id + glyph.id)
-                            imgpath = self.extract_segment(path, glyph, page_image, page_coords)
-                            if imgpath:
-                                filelist.append(imgpath)
-
-        self.logger.info("Training %s from %s on %i file pairs",
-                      self.outputpath,
-                      self.modelpath or 'scratch',
-                      len(filelist))
-        rtrain(filelist, self.modelpath, self.outputpath, self.parameter['ntrain'])
-        deletefiles(filelist)
+                    for glyph in word.get_Glyph():
+                        path = join(self.output_file_grp, f"{page_id}_{region.id}_{line.id}_{word.id}_{glyph.id}")
+                        self.filelist.append(self.extract_segment(path, glyph, page_image, page_coords))
+        # FIXME: PAGE-XML not really needed, find a way around this (raising special exception?)
+        return OcrdPageResult(pcgts)
 
     def extract_segment(self, path, segment, page_image, page_coords):
-        #ground truth
+        gtpath = path + '.gt.txt'
+        imgpath = path + '.png'
+        if exists(gtpath) and exists(imgpath):
+            self.logger.debug(f"Reusing {segment.__class__.__name__} '{segment.id}' file pair")
+            return imgpath
+
         gt = segment.TextEquiv
         if not gt:
             return None
@@ -118,11 +120,10 @@ class OcropyTrain(Processor):
         if not gt or not gt.strip():
             return None
         gt = gt.strip()
-        gtpath = path + '.gt.txt'
         with open(gtpath, "w", encoding='utf-8') as f:
             f.write(gt)
 
-        self.logger.debug("Extracting %s '%s'", segment.__class__.__name__, segment.id)
+        self.logger.debug(f"Extracting {segment.__class__.__name__} '{segment.id}' file pair")
         image, coords = self.workspace.image_from_segment(segment, page_image, page_coords)
 
         if 'binarized' not in coords['features'].split(','):
@@ -132,8 +133,6 @@ class OcropyTrain(Processor):
         # resize image to 48 pixel height
         image = resize_keep_ratio(image)
 
-        #save temp image
-        imgpath = path + '.png'
         image.save(imgpath)
 
         return imgpath
